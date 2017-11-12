361b3f425cfefc615b87491720e7c47e2ff7d67c
Steve Partridge <srpartridge@gmail.com>
2017-11-11 16:09:29 -0800

    Initial commit

---
+++ db.go
@@ -0,0 +1,174 @@
+package db
+
+import (
+	"database/sql"
+	// "errors"
+	"fmt"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+
+	"github.com/propervillains/log"
+)
+
+type Database struct {
+	Type    string
+	Id      string
+	Host    string
+	Port    string
+	Name    string
+	User    string
+	Pass    string
+	SSLMode string // postgres
+	conn    *sql.DB
+	Mock    sqlmock.Sqlmock
+}
+
+var (
+	databases []Database
+)
+
+func (db *Database) Connect() *sql.DB {
+
+	var (
+		host = db.Host
+		err  error
+	)
+	if db.Port != "" {
+		host = host + ":" + db.Port
+	}
+
+	switch db.Type {
+
+	case "mysql":
+		db.conn, err = sql.Open("mysql", fmt.Sprintf(
+			"%s:%s@tcp(%s)/%s?multiStatements=true",
+			db.User,
+			db.Pass,
+			host,
+			db.Name,
+		))
+
+	case "postgres":
+		db.conn, err = sql.Open("postgres", fmt.Sprintf(
+			"host=%s dbname=%s user=%s password='%s' port=%s sslmode=%s",
+			db.Host,
+			db.Name,
+			db.User,
+			db.Pass,
+			db.Port,
+			db.SSLMode,
+		))
+
+	case "mock":
+		db.conn, db.Mock, err = sqlmock.New()
+
+	}
+
+	log.IfError(err)
+
+	return db.conn
+
+}
+
+func Get(id string) *Database {
+
+	for i := range databases {
+		if databases[i].Id == id {
+			return &databases[i]
+		}
+	}
+
+	log.Warnf("Unable to find database by id: %d", id)
+
+	return nil
+}
+
+func Conn(id string) *sql.DB {
+	d := Get(id)
+	if d != nil {
+		if d.conn != nil {
+
+			// TODO:
+			// 		move this to a proactive approach,
+			// 		as not to avoid adding it to the
+			// 		overhead/latency of the call
+			//
+			if err := d.conn.Ping(); err != nil {
+				log.Warnf("Error pinging db: %s", err)
+				if err.Error() == "sql: database is closed" {
+					log.Notice("will attempt to reconnect")
+				}
+				return d.Connect()
+			}
+
+			return d.conn
+		}
+		return d.Connect()
+	}
+	return nil
+}
+
+func Check(id string) error {
+	conn := Conn(id)
+
+	rows, err := conn.Query(`SELECT NOW()`)
+	defer rows.Close()
+	log.IfError(err)
+
+	if err == nil {
+		log.Notice("Successfully connected to DB ID:", id)
+	}
+
+	return err
+
+}
+
+func Add(db Database) {
+
+	for i := range databases {
+		if db.Id == databases[i].Id {
+			log.Warnf("%s exists, overriding with new db", db.Id)
+			databases[i] = db
+			return
+		}
+	}
+
+	_ = db.Connect()
+
+	databases = append(databases, db)
+	log.Info("db.Add", db.Type, db.Id)
+}
+
+func AddMySQL(id, host, port, name, user, pass string) {
+	Add(Database{
+		Type: "mysql",
+		Id:   id,
+		Host: host,
+		Port: port,
+		Name: name,
+		User: user,
+		Pass: pass,
+	})
+}
+
+func AddPostgres(id, host, port, name, user, pass, sslMode string) {
+	Add(Database{
+		Type:    "postgres",
+		Id:      id,
+		Host:    host,
+		Port:    port,
+		Name:    name,
+		User:    user,
+		Pass:    pass,
+		SSLMode: sslMode,
+	})
+}
+
+func AddMock(id string) {
+	Add(Database{
+		Type: "mock",
+		Id:   id,
+	})
+}
---
+++ utils/utils.go
@@ -0,0 +1,120 @@
+package db_utils
+
+import (
+	"database/sql"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func StringToFloatSlice(s string, a []float64) []float64 {
+	r := strings.Trim(s, "{}")
+	if a == nil {
+		a = make([]float64, 0, 10)
+	}
+	for _, t := range strings.Split(r, ",") {
+		i, _ := strconv.ParseFloat(t, 64)
+		a = append(a, i)
+	}
+	return a
+}
+
+func StringToIntSlice(s string) []int {
+	r := strings.Trim(s, "{}")
+	a := make([]int, 0, 10)
+	for _, t := range strings.Split(r, ",") {
+		i, _ := strconv.Atoi(t)
+		a = append(a, i)
+	}
+	return a
+}
+
+func StringToStringSlice(s string) []string {
+	r := strings.Trim(s, "{\"\"}")
+	a := make([]string, 0, 10)
+	for _, s := range strings.Split(r, ",") {
+		a = append(a, s)
+	}
+	return a
+}
+
+func StringSliceToString(sl []string) string {
+	r := "{"
+	total := len(sl)
+	for i := range sl {
+		if sl[i] == "" {
+			continue
+		}
+		r = r + sl[i]
+		if i < (total - 1) {
+			r = r + ","
+		}
+	}
+	return r + "}"
+}
+
+func ResultInterfaceToTime(result interface{}) time.Time {
+	if result != nil {
+		return result.(time.Time)
+	}
+	return time.Time{}
+}
+
+func NullStringToTime(str *sql.NullString) time.Time {
+	if str.Valid {
+		if str.String != "0000-00-00 00:00:00" {
+			t, _ := time.Parse("2006-01-02 15:04:05", str.String)
+			return t
+		}
+	}
+	return time.Time{}
+}
+
+func NullStringToTimePtr(str *sql.NullString) *time.Time {
+	t := NullStringToTime(str)
+	return &t
+}
+
+func NullStringToString(str *sql.NullString) string {
+	if str.Valid {
+		return str.String
+	}
+	return ""
+
+}
+
+func NullFloatToFloat(flt *sql.NullFloat64) float64 {
+	if flt.Valid {
+		return flt.Float64
+	}
+	return 0
+
+}
+
+func NullInt64ToInt64(i *sql.NullInt64) int64 {
+	if i.Valid {
+		return i.Int64
+	}
+	return 0
+}
+
+func NullInt64ToInt32(i *sql.NullInt64) int32 {
+	if i.Valid {
+		return int32(i.Int64)
+	}
+	return 0
+}
+
+func NullInt64ToInt(i *sql.NullInt64) int {
+	if i.Valid {
+		return int(i.Int64)
+	}
+	return 0
+}
+
+func NullBoolToBool(b *sql.NullBool) bool {
+	if b.Valid {
+		return b.Bool
+	}
+	return false
+}
