544b033631cde63f0a6440bc328c4b81096d07e9
Steve Perjesi <steve@perjesi.com>
2019-10-10 14:17:23 -0400

    added return null string helper

--- utils/utils.go
+++ utils/utils.go
@@ -121,3 +121,16 @@ func NullBoolToBool(b *sql.NullBool) bool {
 	}
 	return false
 }
+
+func NullStringIfEmpty(s *string) sql.NullString {
+	if s == nil {
+		return sql.NullString{} 
+	}
+    if len(s) == 0 {
+        return sql.NullString{}
+    }
+    return sql.NullString{
+         String: s,
+         Valid: true,
+    }
+}
